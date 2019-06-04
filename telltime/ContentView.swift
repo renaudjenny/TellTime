1e40f43bd92493a8134d76369b1f571d04e845dc
Renaud Jenny <renaud.jenny@gmail.com>
2019-06-04 23:33:29 +0100

    Initial Commit

---
+++ telltime/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  telltime
+//
+//  Created by Renaud JENNY on 04/06/2019.
+//  Copyright © 2019 Renaud JENNY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
