1a012d3fe3ed4ad89cc54b09a32937d1234803db
Renaud Jenny <renaud.jenny@gmail.com>
2019-09-14 06:36:39 +0100

    build: update to Xcode 11 beta 7
    * Fix bug with double speech when you pressed the rendom button
    * Improve UI

--- telltime/ContentView.swift
+++ telltime/ContentView.swift
@@ -5,6 +5,14 @@ struct ContentView: View {
   @EnvironmentObject var store: Store
 
   var body: some View {
+//    if UIDevice.current.orientation.isLandscape {
+      return self.landscapeBody
+//    } else {
+//      return self.portraitBody
+//    }
+  }
+
+  var portraitBody: some View {
     VStack {
       Clock()
         .padding()
@@ -15,24 +23,32 @@ struct ContentView: View {
         Text("Change time")
       }
     }
-      .onAppear(perform: self.setupTTS)
+  }
+
+  var landscapeBody: some View {
+    HStack {
+      Clock()
+        .padding()
+      VStack {
+        DatePicker("Time", selection: self.$store.date, displayedComponents: [.hourAndMinute])
+        Button(action: {
+          self.changeClockRandomly()
+        }) {
+          Text("Change time")
+        }
+      }
+    }
   }
 
   func changeClockRandomly() {
     let hour = [Int](1...12).randomElement() ?? 0
     let minute = [Int](0...59).randomElement() ?? 0
-    print("", (hour, minute))
-    self.store.hour = hour
-    self.store.minute = minute
-  }
 
-  private func setupTTS() {
-    _ = self.store.willChange
-      .debounce(for: .milliseconds(1500), scheduler: RunLoop.main)
-      .sink { store in
-        TTS(hour: self.store.hour, minute: self.store.minute)
-          .speechTime()
-      }
+    let newDate = Calendar.current.date(
+      byAdding: DateComponents(hour: hour, minute: minute),
+      to: self.store.date
+    )
+    self.store.date = newDate ?? self.store.date
   }
 }
 
--- telltime/Store.swift
+++ telltime/Store.swift
@@ -1,26 +1,19 @@
 import SwiftUI
 import Combine
 
-final class Store: BindableObject  {
-  let willChange = PassthroughSubject<Void, Never>()
+final class Store: ObservableObject  {
+  @Published var date: Date = Date() {
+    didSet {
+      TTS(hour: self.hour, minute: self.minute)
+        .speechTime()
+    }
+  }
 
   var hour: Int {
-    get { Calendar.current.component(.hour, from: self.date) }
-    set {
-      self.date = Calendar.current.date(byAdding: DateComponents(hour: newValue), to: self.date) ?? Date()
-    }
+    Calendar.current.component(.hour, from: self.date)
   }
 
   var minute: Int {
-    get { Calendar.current.component(.minute, from: self.date) }
-    set {
-      self.date = Calendar.current.date(byAdding: DateComponents(minute: newValue), to: self.date) ?? Date()
-    }
-  }
-
-  var date: Date = Date() {
-    willSet {
-      self.willChange.send()
-    }
+    Calendar.current.component(.minute, from: self.date)
   }
 }
