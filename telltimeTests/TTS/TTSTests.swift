fb422ae3530c14fb380d6c0dc0e4b1d732e72a61
Renaud Jenny <renaud.jenny@gmail.com>
2020-01-01 12:58:31 +0000

    test(TTS): add TTS Rate Ratio engine change logic
    * When I change the TTS Rate Ratio the Rate Ratio of Engine is changed
    as well

--- telltimeTests/TTS/TTSTests.swift
+++ telltimeTests/TTS/TTSTests.swift
@@ -9,4 +9,18 @@ class TTSTests: XCTestCase {
     XCTAssertEqual(store.state.tts.speakingProgress, 0)
     XCTAssertEqual(store.state.tts.rateRatio, 1)
   }
+
+  func testWhenIChangeTheTTSRateRatioTheRateRatioOfEngineIsChanged() {
+    let store = Store<App.State, App.Action>(initialState: App.State(), reducer: App.reducer)
+    XCTAssertEqual(store.state.tts.rateRatio, 1)
+    let newRateRatio: Float = 0.75
+    let engineSetRateRatioExpectation = self.expectation(description: "Engine ratio is set")
+    Current.tts.setRateRatio = { rateRatio in
+      XCTAssertEqual(newRateRatio, rateRatio)
+      engineSetRateRatioExpectation.fulfill()
+    }
+    store.send(.tts(.changeRateRatio(newRateRatio)))
+    XCTAssertEqual(newRateRatio, store.state.tts.rateRatio)
+    self.wait(for: [engineSetRateRatioExpectation], timeout: 0.1)
+  }
 }
