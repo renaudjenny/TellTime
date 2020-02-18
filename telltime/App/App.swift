00d19bc745e973c3a22651dfc0c6329f87b9800a
Renaud Jenny <renaud.jenny@gmail.com>
2020-02-18 18:38:17 +0000

    refactor(App): code style - use 4 spaces instead of 2

--- telltime/App/App.swift
+++ telltime/App/App.swift
@@ -2,28 +2,28 @@ import SwiftUI
 import Combine
 
 enum App {
-  struct State {
-    var configuration = Configuration.State()
-    var clock = Clock.State()
-    var tts = TTS.State()
-  }
-
-  enum Action {
-    case configuration(Configuration.Action)
-    case clock(Clock.Action)
-    case tts(TTS.Action)
-  }
-
-  static func reducer(state: inout App.State, action: App.Action) {
-    switch action {
-    case let .configuration(action):
-      Configuration.reducer(state: &state.configuration, action: action)
-    case let .clock(action):
-      Clock.reducer(state: &state.clock, action: action)
-    case let.tts(action):
-      TTS.reducer(state: &state.tts, action: action)
+    struct State {
+        var configuration = Configuration.State()
+        var clock = Clock.State()
+        var tts = TTS.State()
     }
-  }
+
+    enum Action {
+        case configuration(Configuration.Action)
+        case clock(Clock.Action)
+        case tts(TTS.Action)
+    }
+
+    static func reducer(state: inout App.State, action: App.Action) {
+        switch action {
+        case let .configuration(action):
+            Configuration.reducer(state: &state.configuration, action: action)
+        case let .clock(action):
+            Clock.reducer(state: &state.clock, action: action)
+        case let.tts(action):
+            TTS.reducer(state: &state.tts, action: action)
+        }
+    }
 
     #if DEBUG
     static func previewStore(modifyState: (inout App.State) -> Void) -> Store<App.State, App.Action> {
