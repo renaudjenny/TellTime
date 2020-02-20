95b62c0d8c24e8d9a2ce82f23cceb15fb0862901
Renaud Jenny <renaud.jenny@gmail.com>
2020-02-20 17:55:24 +0000

    refactor(App): code style - use 4 spaces instead of 2 part 3

--- telltime/App/SceneDelegate.swift
+++ telltime/App/SceneDelegate.swift
@@ -2,28 +2,28 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    guard let windowScene = scene as? UIWindowScene else { return }
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
 
-    let store = Store<App.State, App.Action>(initialState: App.State(), reducer: App.reducer)
-    let window = UIWindow(windowScene: windowScene)
-    window.rootViewController = UIHostingController(
-      rootView: RootView()
-        .environmentObject(store)
-    )
-    self.window = window
-    window.makeKeyAndVisible()
+        let store = Store<App.State, App.Action>(initialState: App.State(), reducer: App.reducer)
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(
+            rootView: RootView()
+                .environmentObject(store)
+        )
+        self.window = window
+        window.makeKeyAndVisible()
 
-    self.customizeAppearance()
-  }
+        self.customizeAppearance()
+    }
 
-  private func customizeAppearance() {
-    UINavigationBar.appearance().tintColor = UIColor.red
-  }
+    private func customizeAppearance() {
+        UINavigationBar.appearance().tintColor = UIColor.red
+    }
 }
