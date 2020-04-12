64c75f5879c75308547ef48efee7563f5ce0ad89
Renaud Jenny <renaud.jenny@gmail.com>
2020-04-12 18:47:20 +0100

    refactor(Lint): add indentation width as Linter rule

--- telltimeTests/Configuration/ConfigurationViewTests.swift
+++ telltimeTests/Configuration/ConfigurationViewTests.swift
@@ -4,17 +4,17 @@ import SnapshotTesting
 import SwiftUI
 
 class ConfigurationViewTests: XCTestCase {
-  func testConfigurationViews() {
-    let configurationViews = ConfigurationView_Previews.previews
-        .environment(\.clockIsAnimationEnabled, false)
-    let hostingController = UIHostingController(rootView: configurationViews)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
-  }
-
-  func testConfigurationViewsInLandscape() {
-    let configurationViews = ConfigurationViewLandscape_Previews.previews
-        .environment(\.clockIsAnimationEnabled, false)
-    let hostingController = UIHostingController(rootView: configurationViews)
-    assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe(.landscape)))
-  }
+    func testConfigurationViews() {
+        let configurationViews = ConfigurationView_Previews.previews
+            .environment(\.clockIsAnimationEnabled, false)
+        let hostingController = UIHostingController(rootView: configurationViews)
+        assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe))
+    }
+    
+    func testConfigurationViewsInLandscape() {
+        let configurationViews = ConfigurationViewLandscape_Previews.previews
+            .environment(\.clockIsAnimationEnabled, false)
+        let hostingController = UIHostingController(rootView: configurationViews)
+        assertSnapshot(matching: hostingController, as: .image(on: .iPhoneSe(.landscape)))
+    }
 }
