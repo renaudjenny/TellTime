9ba282349df99c39e2bf751ae09916769ff8a127
Renaud Jenny <renaud.jenny@gmail.com>
2020-04-16 18:32:54 +0100

    refactor(TTS): reindent TTS Store code properly

--- telltime/TTS/TTS.swift
+++ telltime/TTS/TTS.swift
@@ -2,21 +2,21 @@ import Foundation
 import Combine
 
 enum TTS {
-  struct State {
-    var isSpeaking = false
-    var speakingProgress = 0.0
-    var rateRatio: Float = 1.0
-  }
+    struct State {
+        var isSpeaking = false
+        var speakingProgress = 0.0
+        var rateRatio: Float = 1.0
+    }
 
-  enum Action {
-    case changeRateRatio(Float)
-    case tellTime(Date)
-    case startSpeaking
-    case stopSpeaking
-    case changeSpeakingProgress(Double)
-    case subscribeToEngineIsSpeaking
-    case subscribeToEngineSpeakingProgress
-  }
+    enum Action {
+        case changeRateRatio(Float)
+        case tellTime(Date)
+        case startSpeaking
+        case stopSpeaking
+        case changeSpeakingProgress(Double)
+        case subscribeToEngineIsSpeaking
+        case subscribeToEngineSpeakingProgress
+    }
 
     struct Environment {
         let engine: TTSEngine
