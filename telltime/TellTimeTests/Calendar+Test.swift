3813af94d212fa83aa9b8de00f7fd9ae32c14f5e
Renaud Jenny <renaud.jenny@gmail.com>
2023-04-12 20:20:47 +0200

    feat(App): remove useless tests

--- telltime/TellTimeTests/Calendar+Test.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Calendar {
-    static var test: Self {
-        var calendar = Self(identifier: .gregorian)
-        calendar.timeZone = TimeZone(secondsFromGMT: 0) ?? calendar.timeZone
-        return calendar
-    }
-}
--- telltime/TellTimeTests/SnapshotTesting+DefaultImage.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-import SnapshotTesting
-
-extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
-    static var defaultImage: Self { image(layout: .device(config: .iPhoneSe)) }
-}
