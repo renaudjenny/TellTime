47618e7e0cc67fd106f974fe66bad2b003c057ec
Renaud Jenny <renaud.jenny@gmail.com>
2023-04-20 09:06:29 +0200

    feat(App): update AppCore with modern TCA approach

--- telltime/TellTime/AppCore.swift
+++ telltime/TellTime/AppCore.swift
@@ -5,133 +5,111 @@ import Speech
 import AVFoundation
 import ComposableArchitecture
 import SpeechRecognizerCore
+import SwiftToTenDependency
 import TTSCore
 
-struct AppState: Equatable {
-    var date: Date = Date()
-    var configuration = Configuration.State()
-    var tts = TTS.State()
-    var speechRecognizer = SpeechRecognizer.State()
-    var isAboutPresented = false
-    var tellTime: String?
-}
+struct App: ReducerProtocol {
+    struct State: Equatable {
+        var date: Date = Date()
+        var configuration = Configuration.State()
+        var tts = TTS.State()
+        var speechRecognizer = SpeechRecognizer.State()
+        var isAboutPresented = false
+        var tellTime: String?
+    }
 
-enum AppAction: Equatable {
-    case setDate(Date)
-    case setRandomDate
-    case configuration(Configuration.Action)
-    case tts(TTS.Action)
-    case speechRecognizer(SpeechRecognizer.Action)
-    case appStarted
-    case presentAbout
-    case hideAbout
-}
+    enum Action: Equatable {
+        case setDate(Date)
+        case setRandomDate
+        case configuration(Configuration.Action)
+        case tts(TTS.Action)
+        case speechRecognizer(SpeechRecognizer.Action)
+        case appStarted
+        case presentAbout
+        case hideAbout
+    }
 
-struct AppEnvironment {
-    var currentDate: () -> Date
-    var randomDate: (Calendar) -> Date
-    var calendar: Calendar
-    var tellTime: (Date, Calendar) -> String
-    var recognizeTime: (String, Calendar) -> Date?
-    var mainQueue: AnySchedulerOf<DispatchQueue>
-}
+    @Dependency(\.date) var date
+    @Dependency(\.calendar) var calendar
+    @Dependency(\.randomDate) var randomDate
+    @Dependency(\.tellTime) var tellTime
+    @Dependency(\.recognizeTime) var recognizeTime
+    @Dependency(\.speechRecognizer) var speechRecognizer
+    @Dependency(\.mainQueue) var mainQueue
 
-let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
-    AnyReducer(Configuration()).pullback(state: \.configuration, action: /AppAction.configuration, environment: { $0 }),
-    AnyReducer(TTS()).pullback(state: \.tts, action: /AppAction.tts, environment: { $0 }),
-    AnyReducer(SpeechRecognizer()).pullback(
-        state: \.speechRecognizer,
-        action: /AppAction.speechRecognizer,
-        environment: { $0 }
-    ),
-    Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
-        struct RecognizedTimeDebounceId: Hashable { }
+    var body: some ReducerProtocol<State, Action> {
+        Scope(state: \.configuration, action: /App.Action.configuration) {
+            Configuration()
+        }
+        Scope(state: \.tts, action: /App.Action.tts) {
+            TTS()
+        }
+        Scope(state: \.speechRecognizer, action: /App.Action.speechRecognizer) {
+            SpeechRecognizer()
+        }
+        Reduce { state, action in
+            struct RecognizedTimeDebounceId: Hashable { }
 
-        switch action {
-        case let .setDate(date):
-            return setDate(date, state: &state, environment: environment)
-        case .setRandomDate:
-            let randomDate = environment.randomDate(environment.calendar)
-            return setDate(randomDate, state: &state, environment: environment)
-        case .appStarted:
-            if state.tellTime == nil {
-                return setDate(state.date, state: &state, environment: environment)
+            switch action {
+            case let .setDate(date):
+                return setDate(date, state: &state)
+            case .setRandomDate:
+                let randomDate = randomDate()
+                return setDate(randomDate, state: &state)
+            case .appStarted:
+                if state.tellTime == nil {
+                    return setDate(state.date, state: &state)
+                }
+                return .none
+            case .presentAbout:
+                state.isAboutPresented = true
+                return .none
+            case .hideAbout:
+                state.isAboutPresented = false
+                return .none
+            case let .speechRecognizer(.setUtterance(utterance)):
+                guard
+                    let utterance = utterance,
+                    let date = recognizeTime(time: utterance, calendar: calendar)
+                else { return .none }
+                speechRecognizer.stopRecording()
+                return setDate(date, state: &state)
+            case .configuration(.binding(\.$speechRateRatio)):
+                @Dependency(\.tts) var tts
+                tts.setRateRatio(state.configuration.speechRateRatio)
+                return .none
+            case .configuration: return .none
+            case .tts: return .none
+            case .speechRecognizer: return .none
             }
-            return .none
-        case .presentAbout:
-            state.isAboutPresented = true
-            return .none
-        case .hideAbout:
-            state.isAboutPresented = false
-            return .none
-        case let .speechRecognizer(.setUtterance(utterance)):
-            guard
-                let utterance = utterance,
-                let date = environment.recognizeTime(utterance, environment.calendar)
-            else { return .none }
-            // TODO: debounce?
-            @Dependency(\.speechRecognizer) var speechRecognizer
-            speechRecognizer.stopRecording()
-            return setDate(date, state: &state, environment: environment)
-        case .configuration(.binding(\.$speechRateRatio)):
-            @Dependency(\.tts) var tts
-            tts.setRateRatio(state.configuration.speechRateRatio)
-            return .none
-        case .configuration: return .none
-        case .tts: return .none
-        case .speechRecognizer: return .none
         }
     }
-)
 
-private func setDate(_ date: Date, state: inout AppState, environment: AppEnvironment) -> EffectTask<AppAction> {
-    state.date = date
-    state.tellTime = environment.tellTime(date, environment.calendar)
-    state.tts.text = state.tellTime ?? ""
-    return .none
+    private func setDate(_ date: Date, state: inout App.State) -> EffectTask<App.Action> {
+        state.date = date
+        state.tellTime = tellTime(date, calendar)
+        state.tts.text = state.tellTime ?? ""
+        return .none
+    }
 }
 
 #if DEBUG
-extension Store where State == AppState, Action == AppAction {
-    static func preview(
-        modifyState: (inout AppState) -> Void = { _ in },
-        modifyEnvironment: (inout AppEnvironment) -> Void = { _ in }
-    ) -> Self {
-        let state: AppState = .preview(modifyState: modifyState)
-        let environment: AppEnvironment = .preview(modifyEnvironment: modifyEnvironment)
-
-        return Self(initialState: state, reducer: appReducer, environment: environment)
+extension Store where State == App.State, Action == App.Action {
+    static func preview(modifyState: (inout App.State) -> Void = { _ in }) -> Self {
+        let state: App.State = .preview(modifyState: modifyState)
+        return Self(initialState: state, reducer: App())
     }
 
     static var preview: Self { preview() }
 }
 
-extension AppState {
+extension App.State {
     static func preview(
-        modifyState: (inout AppState) -> Void = { _ in }
+        modifyState: (inout App.State) -> Void = { _ in }
     ) -> Self {
-        var state = AppState()
+        var state = App.State()
         modifyState(&state)
         return state
-    }
-
-    static var preview: Self { .preview() }
-}
-
-extension AppEnvironment {
-    static func preview(
-        modifyEnvironment: (inout AppEnvironment) -> Void = { _ in }
-    ) -> Self {
-        var environment = AppEnvironment(
-            currentDate: { Date() },
-            randomDate: generateRandomDate,
-            calendar: Calendar.autoupdatingCurrent,
-            tellTime: mockedTellTime,
-            recognizeTime: { _, _ in nil },
-            mainQueue: DispatchQueue.main.eraseToAnyScheduler()
-        )
-        modifyEnvironment(&environment)
-        return environment
     }
 
     static var preview: Self { .preview() }
--- telltime/TellTime/Common/RandomDate.swift
+++ telltime/TellTime/Common/RandomDate.swift
@@ -1,11 +1,43 @@
-import SwiftUI
+import Dependencies
+import Foundation
+import XCTestDynamicOverlay
 
-func generateRandomDate(calendar: Calendar) -> Date {
-    let hour = [Int](1...12).randomElement() ?? 0
-    let minute = [Int](0...59).randomElement() ?? 0
-    return calendar.date(bySettingHour: hour, minute: minute, second: 0, of: .epoch) ?? .epoch
+public struct RandomDate {
+    var randomDate: () -> Date
+
+    public init(randomDate: @escaping () -> Date) {
+        self.randomDate = randomDate
+    }
+
+    public func callAsFunction() -> Date {
+        randomDate()
+    }
+}
+
+private extension RandomDate {
+    static let live = Self {
+        @Dependency(\.calendar) var calendar
+        let hour = [Int](1...12).randomElement() ?? 0
+        let minute = [Int](0...59).randomElement() ?? 0
+        return calendar.date(bySettingHour: hour, minute: minute, second: 0, of: .epoch) ?? .epoch
+    }
+    static let preview = RandomDate.live
+    static let test = Self(randomDate: unimplemented("RandomDate.randomDate"))
 }
 
 private extension Date {
     static var epoch: Date { .init(timeIntervalSince1970: 0) }
+}
+
+private enum RandomDateKey: DependencyKey {
+    static let liveValue = RandomDate.live
+    static let previewValue = RandomDate.preview
+    static let testValue = RandomDate.test
+}
+
+extension DependencyValues {
+    var randomDate: RandomDate {
+        get { self[RandomDateKey.self] }
+        set { self[RandomDateKey.self] = newValue }
+    }
 }
